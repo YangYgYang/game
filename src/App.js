7c6cdec70052beda0092d0c1067c876c50730ea3
Gina Young <ginayoung82@gmail.com>
2025-04-18 17:16:19 +0800

    feat: 沒有偵測輸贏的井字遊戲

--- src/App.js
+++ src/App.js
@@ -1,47 +1,62 @@
 import React, { useState } from 'react';
+import './App.css';
 
-const TodoList = ({ list }) => (
-    <div>
-    {
-        list.map((item) => {
-            return <ul><li>{item}</li></ul>;
-        })
-    }
-    </div>
-);
 
-const Control = ({ onAdd }) => {
-    const [value, setValue] = useState('');
 
-    const onChange = (e) => {
-        setValue(e.target.value);
-    }
-    const addItem = (e) => {
-        onAdd(value);
-        setValue('');
-    }
+const UnitSquare = ({ key,value,changeItem }) => {
+  return (<button className="square" key={key} onClick={changeItem}>{value}</button>)
+}
 
-    return (
-        <>
-            <input value={value} onChange={onChange}/>
-            <button onClick={addItem}>Add</button>
-        </>
-    );
-};
+
+const Title = ({ player }) => {
+  return(<div class="status">Current player: {player}</div>)
+}
 
 const App = () => {
-    const [list, setList] = useState([]);
+    const [player, setList] = useState('X');
+    const [squares, setSquares] = useState(Array(9).fill(null));
 
     const onAdd = (value) => {
-        const newArray = [...list, value];
-        setList(newArray);
+      if(squares[value] != null){
+        throw new Error("No")//這裡要改成alert
+      }
+      squares[value] = player;
+      setSquares(squares);
+
+
+      if(player ==='X'){
+        setList("O");
+      }else{
+        setList("X")
+      }
     }
+
 
     return (
         <>
-            <Control onAdd={onAdd} />
-            <TodoList list={list} />
-            <div>Total: {list.length}</div>
+            <Title player={player} />
+            <div>
+         <div class="game">
+            <div class="game-board">
+
+              <div class="board-row">
+                <UnitSquare key = '0' value = {squares[0]} changeItem={ () => onAdd('0')}/>
+                <UnitSquare key = '1' value = {squares[1]} changeItem={ () => onAdd('1')}/>
+                <UnitSquare key = '2' value = {squares[2]} changeItem={ () => onAdd('2')}/>
+                </div>
+                <div class="board-row">
+                <UnitSquare key = '3' value = {squares[3]} changeItem={ () => onAdd('3')}/>
+                <UnitSquare key = '4' value = {squares[4]} changeItem={ () => onAdd('4')}/>
+                <UnitSquare key = '5' value = {squares[5]} changeItem={ () => onAdd('5')}/>
+                </div>
+                <div class="board-row">
+                <UnitSquare key = '6' value = {squares[6]} changeItem={ () => onAdd('6')}/>
+                <UnitSquare key = '7' value = {squares[7]} changeItem={ () => onAdd('7')}/>
+                <UnitSquare key = '8' value = {squares[8]} changeItem={ () => onAdd('8')}/>
+              </div>
+          </div>
+  </div>
+  </div>
         </>
     );
 }
